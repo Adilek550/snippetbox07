f7962a6240622be1a595b226314aa0b40c6ccca3
Adilek550 <58088550+Adilek550@users.noreply.github.com>
2020-12-30 14:09:34 +0600

    third commit

--- main.go
+++ main.go
@@ -3,21 +3,27 @@ import (
 	"log"
 	"net/http"
 )
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Bro"))
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
 }
-func showBro(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a Bro..."))
-}
-func createBro(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create a new Bro..."))
-}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/hiBro", home)
-	mux.HandleFunc("/Bro", showBro)
-	mux.HandleFunc("/Bro/Brat", createBro)
+	mux.HandleFunc("/hiBro", createSnippet)
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
+
+//$ curl -i -X PUT http://localhost:4000/hiBro
+//HTTP/1.1 405 Method Not Allowed
+//Date: Wed, 30 Dec 2020 07:44:25 GMT
+//Content-Length: 18
+//Content-Type: text/plain; charset=utf-8
+//
+//Method Not Allowed
