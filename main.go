85bd015e8f16b37203fad7c115f018a94b55bbb1
Adilek550 <58088550+Adilek550@users.noreply.github.com>
2020-12-30 15:32:57 +0600

    7 commit

--- main.go
+++ main.go
@@ -1,32 +1,25 @@
 package main
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "This Method Not Allowed", 405)
-		w.Header()["Date"] = nil
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Create a new snippet, Bro..."))
+	fmt.Fprintf(w, "Display a specific snippet with ID: %d...", id)
 }
+
 
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/hiBro", createSnippet)
+	mux.HandleFunc("/hiBro", showSnippet)
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
-
-//$ curl -i -X PUT http://localhost:4000/hiBro
-//HTTP/1.1 405 Method Not Allowed
-//Allow: POST
-//Date: Wed, 30 Dec 2020 07:48:07 GMT
-//Content-Length: 18
-//Content-Type: text/plain; charset=utf-8
-//
-//Method Not Allowed
