7acbca3a8901810fd628039cf587db3098be78d4
Adilek550 <58088550+Adilek550@users.noreply.github.com>
2020-12-30 14:05:12 +0600

    first commit

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ import (
 	"net/http"
 )
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippetbox07"))
+	w.Write([]byte("Hello from Bro"))
+}
+func showBro(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a Bro..."))
+}
+func createBro(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new Bro..."))
 }
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/hiBro", home)
+	mux.HandleFunc("/Bro", showBro)
+	mux.HandleFunc("/Bro/Brat", createBro)
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
