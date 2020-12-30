8a36282ab8a9e113b410cc6a3386502f89727a9b
Adilek550 <58088550+Adilek550@users.noreply.github.com>
2020-12-30 14:10:53 +0600

    4 commit

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 )
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
 		w.WriteHeader(405)
 		w.Write([]byte("Method Not Allowed"))
 		return
@@ -22,7 +23,8 @@ func main() {
 
 //$ curl -i -X PUT http://localhost:4000/hiBro
 //HTTP/1.1 405 Method Not Allowed
-//Date: Wed, 30 Dec 2020 07:44:25 GMT
+//Allow: POST
+//Date: Wed, 30 Dec 2020 07:48:07 GMT
 //Content-Length: 18
 //Content-Type: text/plain; charset=utf-8
 //
