1693dcfdb7e0e0e1ed3f91163618578a60026a64
Adilek550 <58088550+Adilek550@users.noreply.github.com>
2021-01-01 12:56:52 +0600

    9 commit

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,29 +1,42 @@
 package main
 import (
 	"fmt"
+	"html/template" // New import
+	"log" // New import
 	"net/http"
 	"strconv"
+
 )
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Hello from Snippetbox07"))
-}
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
+	ts, err := template.ParseFiles("C:\\Users\\User\\go\\src\\snippetbox\\ui\\html\\html.page.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
 		return
 	}
-	fmt.Fprintf(w, "Display a specific snippet07 with ID %d...", id)
-}
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", 405)
-		return
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
 	}
-	w.Write([]byte("Create a new snippet07..."))
 }
+	func showSnippet(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(r.URL.Query().Get("id"))
+		if err != nil || id < 1 {
+			http.NotFound(w, r)
+			return
+		}
+		fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	}
+	func createSnippet(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.Header().Set("Allow", http.MethodPost)
+			http.Error(w, "Method Not Allowed", 405)
+			return
+		}
+		w.Write([]byte("Create a new snippet..."))
+	}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,8 +6,8 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet07", showSnippet)
-	mux.HandleFunc("/snippet/create07", createSnippet)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
