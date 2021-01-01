f59fd66fa5820db820e3f1204380d0a34f587b35
Adilek550 <58088550+Adilek550@users.noreply.github.com>
2021-01-02 00:03:09 +0600

    13 commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,24 @@
 package main
 import (
-	"flag" // New import
+	"flag"
 	"log"
 	"net/http"
+	"os" // New import
 )
 func main() {
-		addr := flag.String("addr", ":4000", "HTTP network address")
+	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	log.Printf("Starting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
