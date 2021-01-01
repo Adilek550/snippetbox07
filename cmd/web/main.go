171bb6c97a4d07b0a773185c1fc07490ca41190d
Adilek550 <58088550+Adilek550@users.noreply.github.com>
2021-01-01 17:35:51 +0600

    10 commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,10 @@
 package main
+
 import (
-	"log"
 	"net/http"
 )
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-	fileServer := http.FileServer(http.Dir("C:\\Users\\User\\go\\src\\snippetbox\\ui\\static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	mux.Handle("/", &home{})
+
 }
