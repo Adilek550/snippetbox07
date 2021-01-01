927a59f1e42b52c826c88b00935bb6c76bd549dd
Adilek550 <58088550+Adilek550@users.noreply.github.com>
2021-01-01 16:55:28 +0600

    10 commit

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,42 +1,48 @@
-package main
-import (
-	"fmt"
-	"html/template" // New import
-	"log" // New import
-	"net/http"
-	"strconv"
+	package main
 
-)
-func home(w http.ResponseWriter, r *http.Request) {
+	import (
+		"fmt"
+		"html/template"
+		"log"
+		"net/http"
+		"strconv"
+	)
+
+	func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+	http.NotFound(w, r)
+	return
 	}
-	ts, err := template.ParseFiles("C:\\Users\\User\\go\\src\\snippetbox\\ui\\html\\html.page.tmpl")
+	files := []string{
+			"C:\\Users\\User\\go\\src\\snippetbox\\ui\\html\\html.page.tmpl",
+			"C:\\Users\\User\\go\\src\\snippetbox\\ui\\html\\base.layout.tmpl",
+			"C:\\Users\\User\\go\\src\\snippetbox\\ui\\html\\footer.partial.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-		return
+	log.Println(err.Error())
+	http.Error(w, "Internal Server Error", 500)
+	return
 	}
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+	log.Println(err.Error())
+	http.Error(w, "Internal Server Error", 500)
 	}
-}
+	}
 	func showSnippet(w http.ResponseWriter, r *http.Request) {
-		id, err := strconv.Atoi(r.URL.Query().Get("id"))
-		if err != nil || id < 1 {
-			http.NotFound(w, r)
-			return
+			id, err := strconv.Atoi(r.URL.Query().Get("id"))
+			if err != nil || id < 1 {
+				http.NotFound(w, r)
+				return
+			}
+			fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 		}
-		fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-	}
-	func createSnippet(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			w.Header().Set("Allow", http.MethodPost)
-			http.Error(w, "Method Not Allowed", 405)
-			return
+		func createSnippet(w http.ResponseWriter, r *http.Request) {
+			if r.Method != http.MethodPost {
+				w.Header().Set("Allow", http.MethodPost)
+				http.Error(w, "Method Not Allowed", 405)
+				return
+			}
+			w.Write([]byte("Create a new snippet..."))
 		}
-		w.Write([]byte("Create a new snippet..."))
-	}
